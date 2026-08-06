2965918d4679119e9602902a62f2161654b6f621
yashwant-eigenstudio <yashwant.eigenstudio@gmail.com>
2026-08-06 18:35:39 +0530

    added about section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { About6 } from "@/components/section/about6";
 import { Hero202 } from "@/components/section/hero202";
 
 export default function Home() {
   return (
     <>
       <Hero202 />
+      <About6/>
     </>
   );
 }
