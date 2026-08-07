ebda6682462cbe155316e94ab966521f26bc15c6
yashwant-eigenstudio <yashwant.eigenstudio@gmail.com>
2026-08-07 11:02:42 +0530

    feat: initialize home page with hero, about, and feature sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { About6 } from "@/components/section/about6";
 // import { CaseStudies11 } from "@/components/section/case-studies11";
-import { Feature242 } from "@/components/section/feature242";
+// import { Feature242 } from "@/components/section/feature242";
 import { Feature361 } from "@/components/section/feature361";
 import { Hero202 } from "@/components/section/hero202";
 
@@ -10,7 +10,7 @@ export default function Home() {
       <Hero202 />
       <About6 />
       <Feature361 />
-      <Feature242 />
+      {/* <Feature242 /> */}
       {/* <CaseStudies11 /> */}
 
     </>
